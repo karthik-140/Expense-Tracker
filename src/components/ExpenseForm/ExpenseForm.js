8c1bdd20cf0532d4ab568c96a6ac8a9aab90ebf9
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-26 13:29:02 +0530

    form-cmpt-&-signup-using-firebase

---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,103 @@
+import React, { useState, useRef} from 'react';
+
+import classes from './ExpenseForm.module.css';
+
+const ExpenseForm = () => {
+    const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    setIsLoading(true);
+    let url;
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAh3QApboOkimKRo0ivTZo1CkZk4ZpFK4I'
+    } else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAh3QApboOkimKRo0ivTZo1CkZk4ZpFK4I'
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+          return res.json();
+      } else {
+        return res.json().then((data) => {
+          let errorMessage = "Authetication failed";
+          throw new Error(errorMessage);
+        })
+      }
+    })
+    .then((data) => {
+        console.log("User succefully signed up");
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+  }
+
+   return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler} >
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input
+            type='email'
+            id='email'
+            required
+            ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Confirm Password</label>
+          <input
+            type='password'
+            id='confirmpassword'
+            required
+          />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+   )
+}
+
+export default ExpenseForm;
