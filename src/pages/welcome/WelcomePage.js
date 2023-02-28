da7c2035407e0bfa8abc7b856c082782bed976c3
karthik-140 <shanigaramkarthik@gmail.com>
2023-02-28 10:40:14 +0530

    logout-user-&-redirect-to-login-page

--- src/pages/welcome/WelcomePage.js
+++ src/pages/welcome/WelcomePage.js
@@ -1,15 +1,16 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import classes from './WelcomePage.module.css';
 import AuthContext from "../../components/store/auth-context";
 
 const WelcomePage = () => {
     const authCtx = useContext(AuthContext);
+    const history = useHistory();
 
-    const verifyEmailHandler = () =>{
+    const verifyEmailHandler = () => {
         let url = 'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyAh3QApboOkimKRo0ivTZo1CkZk4ZpFK4I'
-        fetch(url,{
+        fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 requestType: "VERIFY_EMAIL",
@@ -18,25 +19,34 @@ const WelcomePage = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-        }).then((res)=>{
-            if(res.ok){
+        }).then((res) => {
+            if (res.ok) {
                 return res.json();
-            }else{
+            } else {
                 let errorMessage = "Failed to Verify Email";
                 throw new Error(errorMessage);
             }
-        }).then((data)=>{
+        }).then((data) => {
             console.log(data);
-        }).catch((err)=>{
+        }).catch((err) => {
             alert(err.message);
         })
+    }
+
+    const logoutHandler = () =>{
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+        history.replace('/auth');
     }
 
     return (
         <>
             <section className={classes.welcome}>
                 <h1>Welcome to Expense Tracker!!!</h1>
-                <p>Your profile is Incomplete.<Link to="/profile">Complete now</Link></p>
+                <div className={classes['actions-logout']}>
+                    <p>Your profile is Incomplete.<Link to="/profile">Complete now</Link></p>
+                    <button onClick={logoutHandler}>Logout</button>
+                </div>
             </section>
             <div className={classes.actions}>
                 <button onClick={verifyEmailHandler}>Verify Email</button>
